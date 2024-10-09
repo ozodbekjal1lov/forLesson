f0ca9768dc251abfda1aa23a91d27fc24bb1c6b1
ozodbek <ojalilov700@gmail.com>
2024-10-09 12:46:26 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,128 @@
+"use strict"
+/* ------------------------------ 01lesson -----------------------------*/
+/* let number = 10;
+const borderColor = "red";
+
+const person = {
+    name: "Ozodbek",
+    surname: "Jalilov"
+}
+
+person.name = "Nodirabegim";
+person.surname = "Jalilova";
+
+
+console.log(person.name);
+console.log(person.surname); */
+
+/* ------------------------------ 02lesson -----------------------------*/
+
+// Data  type number
+/* const number = 5.6;// Number
+
+console.log(8 / 0); // infinity
+console.log(-8 / 0);// -infinity
+
+console.log("Ozod" * 8);// NaN - Not a Number
+
+// Data  type String
+const clientName = "Ozodbek";
+
+// Data type Boolean
+const isComfortable = true
+
+// Data type Null
+console.log(clientAge);
+
+// Data type Undefined
+const und;
+console.log(und); */
+
+/* ------------------------------ 03lesson -----------------------------*/
+
+// Object 
+
+/* const client ={
+//  key : value    
+    age: 30, // number 
+    clothColour:"black", // string
+    isLong: false, // boolean
+}
+
+console.log(client.isLong);
+
+// Array
+//          key: 0   ,    1   ,    2  , 3  , 4 , 5
+const colors = ["red", "green", "blue", 10 , {}, []];
+
+console.log(colors[1]); */
+
+/* ------------------------------ 03lesson -----------------------------*/
+/* alert("hello js")
+
+const place = confirm("Are you here?")
+//confirm gives True or False
+console.log(place);
+//                               question         default 
+const currentPlace =  prompt("Where are you?", "Uzbekistan")
+
+console.log(currentPlace); 
+//           +string ni number qilib beradi        
+const currentAge =  +prompt("How old are you?", "18")
+
+console.log(currentAge + 10); 
+
+const favouriteColor = []
+
+favouriteColor[0] = prompt('What is your favourite color?')
+favouriteColor[1] = +prompt('What is your favourite number?')
+favouriteColor[2] = prompt('What is your favourite meal?')
+
+console.log(favouriteColor); */
+
+/* ------------------------------ 04lesson -----------------------------*/
+/* const chanel = prompt("What's your favourite chanel?")
+
+console.log(`https://youtube.com/${chanel}/videos`);
+
+const user = "Ozod";
+const job = "JavaScript developer"
+
+console.log(`User name is ${user}, He is ${job}` ); */
+
+/* ------------------------------ 05lesson -----------------------------*/
+//               + bu stringni number qilib beradi
+/* console.log(10 + +"10"); */
+
+// Increment Decrement
+/* let incr = 10;
+let decr = 10;
+
+incr++
+decr--
+
+console.log(incr);
+console.log(decr); */
+
+// Qoldiq
+/* console.log(5 % 2); */
+
+// 2ta == faqat qiymatga qaraydi
+/* console.log(5 * 5 == "25"); */
+// 3ta === qiymatga va Data Typega qaraydi
+/* console.log(5 * 5 === "25"); */
+
+// && = va; || = yoki
+
+/* const isAge = true;
+const isClose = false;
+
+console.log(isAge && isClose); */
+
+/* const isAge = true;
+const isClose = true;
+//                   ! bu falseni true , trueni false qilib beradi 
+console.log(!isAge || !isClose); */
+
+
+console.log(2 + 2 * 2 === 8);
